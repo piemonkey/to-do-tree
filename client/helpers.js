24754d5afa9474e63e2e244b61141d18c7a1a0d2
Rich <piemonkey@gmail.com>
2015-08-26 12:23:53 +0100

    Fixed handling of clicking different sub-task buttons

--- client/helpers.js
+++ client/helpers.js
@@ -31,7 +31,9 @@ Template.lists.helpers({
 
 Template.task.helpers({
   childpath: function(task) {
-    var newBreadcrumb = Session.get('breadcrumbs')
+    var newBreadcrumb = Session.get('breadcrumbs');
+    var breadcrumbStubIndex = newBreadcrumb.lastIndexOf(task.parent) + 1;
+    newBreadcrumb = newBreadcrumb.slice(0, breadcrumbStubIndex);
     newBreadcrumb.push(task._id);
     return FlowRouter.path('user-task', {
       userId: Session.get('userId'),
